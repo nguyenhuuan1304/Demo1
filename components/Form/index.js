37f8775e5faad26211732e35bf81024c4bd14e80
Huu An <nguyenhuuan1304@gmail.com>
2023-02-23 10:35:00 +0700

    develop test

--- components/Form/index.js
+++ components/Form/index.js
@@ -33,6 +33,11 @@ const Form = (props) => {
           <Text style={styles.iconAdd}>+</Text>
         </View>
       </TouchableOpacity>
+      <TouchableOpacity onPress={handleAddTask}>
+        <View style={styles.icon}>
+          <Text style={styles.iconAdd}>+</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
